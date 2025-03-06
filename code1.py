d4233efe322053233a3d12920bf4a38990a56092
David Gütter <nahradnig52@gmail.com>
2025-03-06 14:30:49 +0100

    Create code1.py
    bhihzi

---
+++ code1.py
@@ -0,0 +1,12 @@
+cislo1 = int(input("Zadejte první číslo: "))
+cislo2 = int(input("Zadejte druhé číslo: "))
+s = 0
+print("Nelze dělit nulou" if s == 0 else s)
+i = cislo1 + cislo2
+n = cislo1 - cislo2
+s = cislo1 / cislo2
+r = cislo1 * cislo2
+print(i)
+print(n)
+print(s)
+print(r)
